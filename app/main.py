e2611f37e6df35c891a8fd8556a96400149dd245
iamhemanthhs <iam.hemanthhs@gmail.com>
2025-01-24 09:11:37 +0000

    accepting text in tcp connection

--- app/main.py
+++ app/main.py
@@ -6,12 +6,44 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379))
     print("Server is listening on port 6379...")
-    connection, _ = server_socket.accept()
-    print(connection, _)
-    connection.sendall(b"+PONG\r\n")
+    
+    while True:
+        # Accept a new connection
+        connection, address = server_socket.accept()
+        
+        print("Connection established with:", address)
+        connection_should_be_closed = True
+        try:
+            while True:
+                # You can handle communication with the client here
+                data = connection.recv(1000000)
+                if not data:
+                    # If there's no data, the client has closed the connection
+                    break
+                decoded_data = data.decode('utf-8')  # Decoding the bytes into a string
+                decoded_data = decoded_data.strip()
+                
+                print("Received data:", decoded_data)
+                
+                if decoded_data == "exit":
+                    connection.close()
+                    connection_should_be_closed = False
+                    print("Connection closed.")
+                    break
+                
+                # Send a response back to the client
+                connection.sendall(b"+PONG\r\n")
+                
+        except Exception as e:
+            print(f"Error during communication: {e}")
+        
+        finally:
+            # Close the connection after each client interaction
+            if connection_should_be_closed:
+                connection.close()
+                print("Connection closed.")
 
 
 if __name__ == "__main__":
